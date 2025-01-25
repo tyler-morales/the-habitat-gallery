886f1633861f614ba5c51bb10e6fcab6bca61f86
tyler-morales <tylermorales.dev@gmail.com>
2025-01-25 13:48:00 -0600

    Scroll horiontally

--- app/page.js
+++ app/page.js
@@ -1,16 +1,31 @@
+"use client";
+import { useRef } from "react";
+
 export default function Home() {
+  const scrollRef = useRef(null);
+
+  // Scroll Horizontally
+  const handleScroll = (event) => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollLeft += event.deltaY;
+    }
+  };
   return (
-    <main className="bg-white h-screen flex flex-col overflow-x-scroll no-scrollbar">
+    <main
+      ref={scrollRef}
+      onWheel={handleScroll}
+      className="bg-white h-screen flex flex-col overflow-x-scroll no-scrollbar max-h-100vh"
+    >
       {/* 🚪 Rooms  */}
-      <div className="flex w-[200vw] h-[80vh] flex-shrink-0">
+      <div className="flex w-[170vw] h-[80vh] flex-shrink-0">
         {/* Room 2 */}
-        <section className="bg-blue-600 h-full w-[100vw] flex-shrink-0"></section>
+        <section className="bg-blue-600 h-full w-[70vw] flex-shrink-0"></section>
         {/* Room 2 */}
         <section className="bg-red-600 h-full w-[100vw] flex-shrink-0"></section>
       </div>
 
       {/* Floor */}
-      <section className="bg-green-600 h-[20vh] w-[200vw] flex-shrink-0"></section>
+      <section className="bg-green-600 h-[20vh] w-[170vw] flex-shrink-0"></section>
     </main>
   );
 }
