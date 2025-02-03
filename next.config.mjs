2c485a421850868e179660e4b82068562c11b347
tyler-morales <tylermorales.dev@gmail.com>
2025-02-02 21:35:29 -0600

    Configure output settings and set base path for production environment in next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "/habitat-gallery" : "",
   images: {
     domains: ["wallpaperbat.com"], // Allow external images from this domain
   },
